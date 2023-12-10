3f7d272881103e1f97d9191d729e70f7578eb5a7
MD Shabbir Hossain <shabbirhossain660@gmail.com>
2023-12-10 20:11:37 +0600

    implement params and searchParams

--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -1,9 +1,10 @@
 
-const dynamicPage = ({params}) => {
-    console.log(params);
+const dynamicPage = ({params, searchParams}) => {
+    console.log(searchParams);
     return (
         <div>
-          <h1>this is dynamic page:{params.id} </h1>
+          <h1>this is dynamic page: {params.id} </h1>
+           <h2>searched by: {searchParams.category} & {searchParams.price} </h2>
         </div>
     );
 };
