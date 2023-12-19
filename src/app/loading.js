8dcafb6b0ebe6ed291cc28ad61bb78a5f4ad92f8
MD Shabbir Hossain <shabbirhossain660@gmail.com>
2023-12-19 22:58:11 +0600

    loading page creation

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div>
+            <h1 className='text-3xl font-extrabold'>Loading...</h1>
+        </div>
+    );
+};
+
+export default Loading;
