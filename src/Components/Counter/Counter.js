16af7dade372641fd2cc786b51389e49d18f6d53
MD Shabbir Hossain <shabbirhossain660@gmail.com>
2024-01-01 00:02:12 +0600

    implement disyUi in nextjs

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -1,20 +1,20 @@
 import { useState } from "react";
-
+import { Button } from 'flowbite-react';
 
 const Counter = () => {
   // throw new Error;
   const [count, setCount] = useState(0);
   return (
-    <div>
+    <div className="max-w-7xl mx-auto flex flex-wrap gap-2 font-serif">
       <h2>Counter = {count}</h2>
-      <button
-        className="btn primary bg-white ml-4 p-[10px] text-green-500"
+      <button 
+      className="btn glass"
         onClick={() => setCount(count + 1)}
       >
         Increase
       </button>
-      <button
-        className="btn primary bg-white ml-4 p-[10px] text-red-500"
+      <button 
+        className="btn glass"
         onClick={() => setCount(count - 1)}
       >
         Decrease
